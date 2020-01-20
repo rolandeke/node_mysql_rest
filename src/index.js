5559823a44178c8b0b08f928335c299828820382
rolandeke <rolandeke49@gmail.com>
2020-01-20 19:00:55 +0000

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, (err) {
+    if (err) throw err;
+
+    console.log(`Server Running on Port: ${PORT}`)
+})
