efcd4adc2b037e2a8b1c104c3a7a00a95d6ec68d
rolandeke <rolandeke49@gmail.com>
2020-01-20 20:26:27 +0000

    Added delete route for deleting an employee

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -3,6 +3,7 @@ const Employees = require('../models/Employees')
 
 
 
+//GET: ALL EMPLOYEES
 router.get('/', (req, res) => {
 
     const emp = Employees
@@ -18,6 +19,7 @@ router.get('/', (req, res) => {
     })
 })
 
+//GET: ONE EMPLOYEE
 router.get('/:id', (req, res) => {
     const emp = Employees.find(emp => emp.id == parseInt(req.params.id))
     if (!emp) {
@@ -33,6 +35,22 @@ router.get('/:id', (req, res) => {
     }
 })
 
+router.delete('/:id', (req, res) => {
 
+    const {
+        id
+    } = req.params
+    const empIndex = Employees.findIndex(emp => emp.id == parseInt(id))
+    if (empIndex < 0) {
+        console.log("Invalid Id")
+    } else {
+        const deletedEmp = Employees.splice(empIndex, 1)
+        console.log(deletedEmp)
+        res.json(Employees)
+    }
+
+
+
+})
 
 module.exports = router
