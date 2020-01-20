3e92a8f2d0f806f36043b7c363d3710787bea4aa
rolandeke <rolandeke49@gmail.com>
2020-01-20 19:30:53 +0000

    Added a route to get all Employees and a Single Employee

---
+++ src/models/Employees.js
@@ -0,0 +1,20 @@
+const Employees = [
+
+    {
+        id: 1,
+        name: "John Simbo",
+        dept: "Marketing"
+    },
+    {
+        id: 2,
+        name: "John Kamara",
+        dept: "Graphics"
+    },
+    {
+        id: 3,
+        name: "Jane Conteh",
+        dept: "Logistics"
+    }
+]
+
+module.exports = Employees
--- src/routes/employees.js
+++ src/routes/employees.js
@@ -1,10 +1,38 @@
 const router = require('express').Router()
+const Employees = require('../models/Employees')
 
 
 
 router.get('/', (req, res) => {
-    res.send("<h1>All Employees</h1>")
+
+    const emp = Employees
+
+    if (!emp) return res.json({
+        err: "No Recoreds Found",
+        data: emp
+    })
+
+    res.json({
+        err: "",
+        data: emp
+    })
 })
+
+router.get('/:id', (req, res) => {
+    const emp = Employees.find(emp => emp.id == parseInt(req.params.id))
+    if (!emp) {
+        res.status(404).json({
+            err: "Employee Not Found",
+            data: emp
+        })
+    } else {
+        res.json({
+            err: "",
+            data: emp
+        })
+    }
+})
+
 
 
 module.exports = router
